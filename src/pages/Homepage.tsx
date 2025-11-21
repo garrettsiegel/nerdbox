e42fe26e496275862e17e08461ecce90a091e59b
garrettsiegel <garrett@garrettsiegel.com>
2025-11-21 14:25:12 -0500

    react-lzy-img install

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,3 +1,4 @@
+import { LazyImage } from "react-lzy-img"
 import { Intro } from "../components/Intro"
 import { LatestCourses } from "../components/LatestCourses"
 
@@ -6,6 +7,15 @@ export const Homepage = () => {
     <div className="h-full">
       <Intro />
       <LatestCourses />
+
+      <LazyImage
+        src="https://a.espncdn.com/photo/2025/0716/cursedfans_16x9.png"
+        alt="Sports Misery Index Project"
+        blurhash="LHPFQL%#_Os9rYOrt,r@@FPUMdcE"
+        className="w-1/2 h-auto rounded-xl shadow-lg overflow-hidden"
+        onLoad={() => console.log('Image loaded!')}
+
+      />
     </div>
   )
 }
